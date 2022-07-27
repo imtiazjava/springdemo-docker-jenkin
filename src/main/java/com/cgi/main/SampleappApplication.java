73bfa98ad87e27c09db0611dd29325cb8b5e38d8
imtiaz <imtiazjavatrainer@gmail.com>
2022-07-27 15:41:05 +0530

    fc

---
+++ src/main/java/com/cgi/main/SampleappApplication.java
@@ -0,0 +1,14 @@
+package com.cgi.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleappApplication.class, args);
+		System.out.println("hello1");
+	}
+
+}
